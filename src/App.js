3fc414df035e814ac3488b01106ec7ba664c6bdf
anil843324 <anilkrsingh14200@gmail.com>
2022-06-16 17:28:23 +0530

    Header part almost done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
+
+import { FormControl, Select, MenuItem } from '@mui/material';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [countries, setCountires] = useState([])
+
+
+  useEffect(() => {
+
+    const getCountriesData = async () => {
+
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+
+          const countries = data.map((country) => (
+            {
+              name: country.country, // United State, United Kingdom
+              value: country.iso3  // UK ,USA , IND
+
+            }
+
+          ))
+          setCountires(countries)
+
+        })
+    }
+    getCountriesData()
+
+  }, [])
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="app_header">
+        <h1>COVID-19 TRACKER</h1>
+
+        <FormControl className='app_dropdown'>
+
+          <Select
+            variant='outlined'
+             value={"abc"}
+             
+          >
+            {
+              countries.map( (country,index) => (
+                <MenuItem key={index} value={country.value}>{country.name}</MenuItem>
+              ))
+            }
+          </Select>
+        </FormControl>
+      </div>
+      {/* Header  */}
+      {/* Title + select input dropdown filed */}
+
+
+      {/* InfoBoxs */}
+      {/* InfoBoxs */}
+      {/* InfoBoxs */}
+
+
+      {/* Table */}
+      {/* Graph */}
+
+
+      {/* Map */}
+
+
     </div>
   );
 }
